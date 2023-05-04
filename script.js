7317a6106455042c143dc3151b9c67127fc9122b
TunedayCodes <fadipetunde5@gmail.com>
2023-05-04 06:41:27 +0100

    New updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let colors = [
+var colors = [
    '#16a085',
    '#27ae60',
    '#2c3e50',
@@ -11,42 +11,41 @@ let colors = [
    '#BDBB99',
    '#77B1A9',
    '#73A857'
-];
-
-let currentQuote = '';
-let currentAuthor = '';
-
-$(document).ready(function (){
+ ];
+ var currentQuote = '';
+ var currentAuthor = '';
+ 
+ $(document).ready(function () {
    getQuotes().then(() => {
-         getQuotes();
+     getQuote();
    });
-
+ 
    $('#new-quote').on('click', getQuote);
-});
-
-function getQuotes() {
+ });
+ 
+ function getQuotes() {
    return $.ajax({
-      headers: {
-         Accept: 'application/json'
-      },
-      url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
-      success: function (jsonQuotes) {
-         if (typeof jsonQuotes === 'string') {
-            quotesData = JSON.parse(jsonQuotes);
-            console.log('quotesData');
-            console.log(quotesData);
-         }
-      }
+     headers: {
+       Accept: 'application/json'
+     },
+     url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+     success: function (jsonQuotes) {
+       if (typeof jsonQuotes === 'string') {
+         quotesData = JSON.parse(jsonQuotes);
+         console.log('quotesData');
+         console.log(quotesData);
+       }
+     }
    });
-}
-
-function getQuote(){
+ }
+ 
+ function getRandomQuote() {
    return quotesData.quotes[
-      Math.floor(Math.random() * quotesData.quotes.length)
+     Math.floor(Math.random() * quotesData.quotes.length)
    ];
-}
-
-function getQuote() {
+ }
+ 
+ function getQuote() {
    let randomQuote = getRandomQuote();
  
    currentQuote = randomQuote.quote;
@@ -57,38 +56,40 @@ function getQuote() {
      'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
        encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
    );
-
+ 
    $('#tumblr-quote').attr(
-      'href',
-      'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
-        encodeURIComponent(currentAuthor) +
-        '&content=' +
-        encodeURIComponent(currentQuote) +
-        '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
-    );
-
-    $('.quote-text').animate({ opacity: 0 }, 500, function () {
-      $(this).animate({ opacity: 1 }, 500);
-      $('#text').text(randomQuote.quote);
-    });
-  
-    $('.quote-author').animate({ opacity: 0 }, 500, function () {
-      $(this).animate({ opacity: 1 }, 500);
-      $('#author').html(randomQuote.author);
-    });
-  
-    var color = Math.floor(Math.random() * colors.length);
-    $('html body').animate(
-      {
-        backgroundColor: colors[color],
-        color: colors[color]
-      },
-      1000
-    );
-    $('.button').animate(
-      {
-        backgroundColor: colors[color]
-      },
-      1000
-    );
-  }
+     'href',
+     'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+       encodeURIComponent(currentAuthor) +
+       '&content=' +
+       encodeURIComponent(currentQuote) +
+       '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+   );
+ 
+   $('.quote-text').animate({ opacity: 0 }, 500, function () {
+     $(this).animate({ opacity: 1 }, 500);
+     $('#text').text(randomQuote.quote);
+   });
+ 
+   $('.quote-author').animate({ opacity: 0 }, 500, function () {
+     $(this).animate({ opacity: 1 }, 500);
+     $('#author').html(randomQuote.author);
+   });
+ 
+   var color = Math.floor(Math.random() * colors.length);
+   $('html body').animate(
+     {
+       backgroundColor: colors[color],
+       color: colors[color]
+     },
+     1000
+   );
+   $('.button').animate(
+     {
+       backgroundColor: colors[color]
+     },
+     1000
+   );
+ }
+ 
+ 
